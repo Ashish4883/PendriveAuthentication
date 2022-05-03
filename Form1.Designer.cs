d3b25f1c13e3024130fc5b99bde10aec7442cb7d
Ashish4883 <aashishmishra0016@gmail.com>
2022-05-03 13:45:19 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace PendriveAuthentication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Door Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(193, 129);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PendriveAuthentication
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Pendrive"].ConnectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+
+            ManagementObjectSearcher theSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            foreach (ManagementObject currentObject in theSearcher.Get())
+            {
+                ManagementObject theSerialNumberObjectQuery = new ManagementObject("Win32_PhysicalMedia.Tag='" + currentObject["DeviceID"] + "'");
+                Pendriveid = theSerialNumberObjectQuery["SerialNumber"].ToString();
+            }
+            //MessageBox.Show(Pendriveid);
+
+        }
+
+        protected string SendEmail(string toAddress, string subject, string body)
+        {
+            string result = "Message Sent Successfully..!!";
+            string senderID = "bank.manager7894@gmail.com";// use sender’s email id here..
+            const string senderPassword = "7894@Project"; // sender password here…
+            try
+            {
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com", // smtp server address here…
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new System.Net.NetworkCredential(senderID, senderPassword),
+                    Timeout = 30000,
+                };
+                MailMessage message = new MailMessage(senderID, toAddress, subject, body);
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+                result = "Error sending email.!!!";
+            }
+            return result;
+        }
+        string Otp = String.Empty;
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            Otp = serialPort1.ReadLine();
+            
+            ManagementObjectSearcher theSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            foreach (ManagementObject currentObject in theSearcher.Get())
+            {
+                ManagementObject theSerialNumberObjectQuery = new ManagementObject("Win32_PhysicalMedia.Tag='" + currentObject["DeviceID"] + "'");
+                string Pendriveid = theSerialNumberObjectQuery["SerialNumber"].ToString();
+                SqlDataAdapter da = new SqlDataAdapter("select * from tblpendrive where Pendriveid='"+Pendriveid+"'", cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    
+                    SendEmail("bank.customer7894@gmail.com", "Otp", Otp);
+                    if (serialPort1.IsOpen)
+                    {
+                        serialPort1.Close();
+                    }
+                    serialPort1.PortName = "COM3";
+                    serialPort1.BaudRate = 9600;
+                    serialPort1.Open();
+                    serialPort1.Write("1");
+                    SqlCommand cmd = new SqlCommand("insert into TblLog(KeyId) values('"+Pendriveid+"')", cn);
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+                else
+                {
+                    SendEmail("bank.customer7894@gmail.com", "Unauthorised Access", "Please Provide The Key Required");
+                    MessageBox.Show("Invalid Key");
+                }
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+            serialPort1.Write("1");
+
+        }
+        string Pendriveid = String.Empty;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+            serialPort1.Write("2");
+
+            ManagementObjectSearcher theSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'");
+            foreach (ManagementObject currentObject in theSearcher.Get())
+            {
+                ManagementObject theSerialNumberObjectQuery = new ManagementObject("Win32_PhysicalMedia.Tag='" + currentObject["DeviceID"] + "'");
+                 Pendriveid = theSerialNumberObjectQuery["SerialNumber"].ToString();
+            }
+
+            SqlCommand cmd = new SqlCommand("update TblLog set CLoseDatetime='" + DateTime.Now+ "' where KeyId='" + Pendriveid + "'", cn);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+    }
+}
